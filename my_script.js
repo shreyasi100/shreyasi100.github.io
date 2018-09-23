2df9c64cdec455fcb2c7121e367e7f4760af11f3
Shreyasi Chaudhary <shreyasi@Shreyasis-MacBook-Pro.local>
2018-09-23 00:15:07 -0700

    made oicture responsive

--- my_script.js
+++ my_script.js
@@ -54,3 +54,12 @@ var TxtRotate = function(el, toRotate, period) {
     css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
     document.body.appendChild(css);
   };
+
+  function myFunction(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
